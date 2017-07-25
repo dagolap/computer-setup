5695f303fd2296784ca437ed54fb829eb4f800d6
Dag Olav Prestegarden <dagolav@prestegarden.com>
2017-07-25 14:48:53 +0200

    Using Ctrl+n/p to scroll company suggestions

--- roles/personal-dotfiles/files/dotfiles/.emacs.d/customizations/editing.el
+++ roles/personal-dotfiles/files/dotfiles/.emacs.d/customizations/editing.el
@@ -74,6 +74,9 @@
 ;; Auto completion
 (add-hook 'after-init-hook 'global-company-mode)
 (global-set-key (kbd "<C-return>") 'company-complete)
+(with-eval-after-load 'company
+  (define-key company-active-map (kbd "C-n") #'company-select-next)
+  (define-key company-active-map (kbd "C-p") #'company-select-previous))
 
 ;; Snippets everywhere
 (yas-global-mode t)
