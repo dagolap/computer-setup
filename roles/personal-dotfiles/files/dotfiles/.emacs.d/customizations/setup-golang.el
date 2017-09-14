a4ff5820b816c23f83051e0408a06c83eb21ade3
Dag Olav Prestegarden <dagolav@prestegarden.com>
2017-09-14 16:34:47 +0200

    Fixing golang setup so it doesnt ruin tab indentation for other files

--- roles/personal-dotfiles/files/dotfiles/.emacs.d/customizations/setup-golang.el
+++ roles/personal-dotfiles/files/dotfiles/.emacs.d/customizations/setup-golang.el
@@ -1,5 +1,5 @@
-(defun my-go-mode-hook ()
-  (setq tab-width 2 indent-tabs-mode 1)
-  (local-set-key (kbd "M-." 'godef-jump))
-  (add-hook 'before-save-hook 'gofmt-before-save))
-(add-hook 'before-save-hook 'my-go-mode-hook)
+(add-hook 'go-mode-hook
+          (lambda ()
+            (setq tab-width 2 indent-tabs-mode 1)
+            (local-set-key (kbd "M-.") 'godef-jump)
+            (add-hook 'before-save-hook 'gofmt-before-save)))
