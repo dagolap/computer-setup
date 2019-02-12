7d135f2b7b339264b63cdf52f2a7c36dff75dd29
Dag Olav Prestegarden <dagolav@prestegarden.com>
2019-02-12 19:04:04 +0100

    Windows provisioning
    - PowerShell stuff for DSC-installation of lots of apps.
    - ctrl-caps-swap

---
+++ windows/deploy-basics.ps1
@@ -0,0 +1,98 @@
+﻿Configuration deployBasics
+{
+    Import-DscResource -Module cChoco
+    Node "localhost"
+    {
+        LocalConfigurationManager
+        {
+            DebugMode = "ForceModuleImport"
+        }
+
+        cChocoInstaller installChoco
+        {
+            InstallDir = "c:\choco"
+        }
+
+        cChocoPackageInstallerSet installBrowsers
+        {
+            Ensure = "Present"
+            Name = @(
+                "googlechrome"
+                "firefox"
+            )
+            DependsOn = "[cChocoInstaller]installChoco"
+        }
+
+        cChocoPackageInstallerSet installProductivity
+        {
+            Ensure = "Present"
+            Name = @(
+                "foxitreader"
+                "libreoffice-fresh"
+                "putty"
+                "winscp"
+                "vscode"
+                "cmder"
+                "git"
+                "7zip"
+                "nodejs"
+                "keepassxc"
+                "synctrayzor"
+                "deluge"
+                "altdrag"
+
+            )
+            DependsOn = "[cChocoInstaller]installChoco"
+        }
+
+        cChocoPackageInstallerSet installMedia
+        {
+            Ensure = "Present"
+            Name = @(
+                "mpv"
+                "vlc"
+                "gimp"
+                "krita"
+                "blender"
+                "inkscape"
+                "darktable"
+                "lmms"
+                "audacity"
+                "handbrake"
+                "virtualbox"
+                "obs-studio"
+                "greenshot"
+                "jcpicker"
+            )
+            DependsOn = "[cChocoInstaller]installChoco"
+        }
+
+        cChocoPackageInstallerSet installSystemUtils
+        {
+            Ensure = "Present"
+            Name = @(
+                "sysinternals"
+                "ccleaner"
+                "geforce-experience"
+
+            )
+            DependsOn = "[cChocoInstaller]installChoco"
+        }
+
+        cChocoPackageInstallerSet installGaming
+        {
+            Ensure = "Present"
+            Name = @(
+                "steam"
+                "epicgameslauncher"
+                "origin"
+                "uplay"
+            )
+            DependsOn = "[cChocoInstaller]installChoco"
+        }
+    }
+}
+
+deployBasics
+
+Start-DscConfiguration .\deployBasics -wait -Verbose -force
---
+++ windows/prepare-deployment.ps1
@@ -0,0 +1,8 @@
+﻿# Allow running locally created scripts
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
+
+# First as admin, setup winrm for remote management.
+winrm quickconfig
+
+# Install Chocolatey DSC Module
+Install-Module -Name cChoco -Force
